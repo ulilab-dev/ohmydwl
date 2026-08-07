b18cf5fdccd5ad59f0f0f47023dc2850aafdd3fe
ulil <ullia5733@gmail.com>
2026-08-07 21:29:55 +0700

    update

--- stuff/config.def.h
+++ stuff/config.def.h
@@ -145,10 +145,10 @@ static const Key keys[] = {
 	{ MODKEY,                    XKB_KEY_Return,      spawn,            {.v = termcmd} },
 	{ MODKEY,                    XKB_KEY_e,           spawn,            {.v = file} },
 	{ MODKEY,                    XKB_KEY_w,           spawn,            SHCMD("waypaper") },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_t,           spawn,            SHCMD("~/.config/waybar/switch.sh") }
+	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_t,           spawn,            SHCMD("~/.config/waybar/switch.sh") },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_w,           spawn,            SHCMD("~/.config/waybar/toggle_waybar.sh") },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_p,           spawn,            SHCMD("hyprshot -m region -o ~/Pictures/Screenshots") },
-	{ MODKEY,                    XKB_KEY_v,           spawn,            SHCMD("~/.config/rofi/clipboard.sh") }
+	{ MODKEY,                    XKB_KEY_v,           spawn,            SHCMD("~/.config/rofi/clipboard.sh") },
 	{ MODKEY,                    XKB_KEY_period,      spawn,            SHCMD("rofi -show emoji -config ~/.config/rofi/emoji.rasi") },
 	
 /*	{ MODKEY,                    XKB_KEY_j,           focusstack,       {.i = +1} },
